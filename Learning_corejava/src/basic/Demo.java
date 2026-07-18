d5e38cc974e704bd1828cc319f6611bc181847d3
chintan <chintan@gmail.com>
2026-07-18 09:10:34 +0530

    all

--- Learning_corejava/src/basic/Demo.java
+++ Learning_corejava/src/basic/Demo.java
@@ -3,7 +3,15 @@ package basic;
 public class Demo {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");
+//		System.out.println("Hello");
+		
+		int add123$_ = 10;
+		int abc =10;
+		int id = 10;
+		
+		String myFirstName = "omprasad";
+		
+
 		
 	}
 }
