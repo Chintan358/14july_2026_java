72f45de198ad68f84592976609cc95d7c5588ccf
chintan <chintan@gmail.com>
2026-08-08 08:23:58 +0530

    all

---
+++ Learning_corejava/src/A005_Pattern/P5.java
@@ -0,0 +1,21 @@
+package A005_Pattern;
+
+public class P5 {
+		public static void main(String[] args) {
+			
+			
+//			*****
+//			 ****
+//			  ***
+//			   **
+//			    *
+			
+//			   *
+//			  * *
+//			 * * *
+//			* * * *
+//		   * * * * *
+			
+			
+		}
+}
